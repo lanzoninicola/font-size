ff470bfd0e8e3787b634da6378d5110dd78e8120
Nicola <lanzoni.nicola@gmail.com>
2022-04-13 19:06:56 -0300

    Console.log data from message

--- public/messages/app.js
+++ public/messages/app.js
@@ -3,6 +3,7 @@
     window.addEventListener(
       "message",
       (event) => {
+        console.log(event.data);
         if (
           event.origin !== "https://font-size-eight.vercel.app" ||
           event.origin !== "http://172.24.64.1:3000"
