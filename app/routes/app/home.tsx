8a33d9fa91583d310fac1d4cf4dcb5e62e9945a3
Nicola <lanzoni.nicola@gmail.com>
2022-05-03 10:45:18 -0300

    Added HOME APP subroute

---
+++ app/routes/app/home.tsx
@@ -0,0 +1,5 @@
+import { Heading } from "@chakra-ui/react";
+
+export default function AppHome() {
+  return <Heading color="white">Welcome</Heading>;
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,10 @@
-import { Button, Center, Flex, Grid, Heading } from "@chakra-ui/react";
-import { Link, LoaderFunction, redirect } from "remix";
-import VStackBox from "~/components/shared/vstack-wrapper";
+import { LoaderFunction, redirect } from "remix";
 
 export const loader: LoaderFunction = async () => {
-  // TODO: loading the google fonts
-
-  return null;
+  let response = redirect("/app/home");
+  return response;
 };
 
 export default function Index() {
-  return (
-    <Flex w="100%" minH="100vh" justify="center" align={"center"}>
-      <VStackBox m="auto" w="max-content" gap="1rem">
-        <Heading
-          as="h1"
-          size={"4xl"}
-          color="white"
-          lineHeight={"1"}
-          letterSpacing={-4}
-        >
-          Welcome to
-          <br /> Font Scale
-        </Heading>
-        <Heading as="h2" size={"lg"} color="white" fontWeight={400}>
-          This tool will help you choose <br />
-          the right font size for your project
-        </Heading>
-        <Link to="/app">
-          <Button>START</Button>
-        </Link>
-      </VStackBox>
-    </Flex>
-  );
+  return <></>;
 }
