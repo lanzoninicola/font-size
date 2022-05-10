fe517790f7956ffe51f7ccadf08de8f452ec9681
Nicola <lanzoni.nicola@gmail.com>
2022-05-10 18:22:16 -0300

    Add notification with the breakpoint is selected the first time

--- app/context/type-scale-calculator-form/hooks/useCurrentBreakpointIdSelector.ts
+++ app/context/type-scale-calculator-form/hooks/useCurrentBreakpointIdSelector.ts
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useContextSelector } from "use-context-selector";
 import { BreakpointId } from "~/context/breakpoint-builder/interfaces";
 import usePreviewWindowsSelector from "~/context/preview/hooks/usePreviewWindowsSelector";
+import useNotifications from "~/domain/app/useNotifications";
 import { TypeScaleCalculatorFormContextData } from "../type-scale-calculator-form-context";
 
 export default function useCurrentBreakpointIdSelector() {
+  const [foo, setFoo] = useState("");
+
   const currentBreakpointId = useContextSelector(
     TypeScaleCalculatorFormContextData,
     (ctx) => ctx?.currentBreakpointId
@@ -16,6 +19,7 @@ export default function useCurrentBreakpointIdSelector() {
   );
 
   const { actions: previewWindowsActions } = usePreviewWindowsSelector();
+  const { infoNotification } = useNotifications();
 
   const actions = {
     TYPE_SCALE_CALCULATOR__CHANGE_BREAKPOINT: {
@@ -24,11 +28,19 @@ export default function useCurrentBreakpointIdSelector() {
   };
 
   function changeCurrentBreakpoint(breakpointId: BreakpointId) {
+    const prevState = currentBreakpointId;
+
     setCurrentBreakpointId(breakpointId);
 
     previewWindowsActions.PREVIEW_WINDOWS__SELECTED_BREAKPOINTS.dispatch(
       breakpointId
     );
+
+    if (prevState === "") {
+      const message =
+        "The smallest and largest device in the preview area was loaded according to the selected breakpoint.";
+      infoNotification(message);
+    }
   }
 
   useEffect(() => {
