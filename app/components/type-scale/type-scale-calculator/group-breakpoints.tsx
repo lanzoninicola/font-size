eb7c2daa466d68b5b1c423de2718b97d5a29153d
Nicola <lanzoni.nicola@gmail.com>
2022-05-11 14:34:37 -0300

    Update headings

--- app/components/type-scale/type-scale-calculator/group-breakpoints.tsx
+++ app/components/type-scale/type-scale-calculator/group-breakpoints.tsx
@@ -1,29 +1,22 @@
-import { Heading } from "@chakra-ui/react";
 import BreakpointsPicker from "~/components/shared/breakpoints-picker";
 import VStackBox from "~/components/shared/vstack-wrapper";
 import useCurrentBreakpointIdSelector from "~/context/type-scale-calculator-form/hooks/useCurrentBreakpointIdSelector";
 
+import { FormHeading } from "./form/form-headings";
+
 export default function GroupBreakpoints() {
   const { currentBreakpointId, actions } = useCurrentBreakpointIdSelector();
 
+  function onChangeBreakpoint(e: React.ChangeEvent<HTMLInputElement>) {
+    actions.TYPE_SCALE_CALCULATOR__CHANGE_BREAKPOINT.dispatch(e.target.value);
+  }
+
   return (
     <VStackBox spacing={2}>
-      <Heading
-        as="h4"
-        fontSize={"xs"}
-        color="secondary.300"
-        textTransform={"uppercase"}
-        letterSpacing={1}
-      >
-        1. Select a breakpoint
-      </Heading>
+      <FormHeading>1. Select a breakpoint</FormHeading>
       <BreakpointsPicker
         size="sm"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          actions.TYPE_SCALE_CALCULATOR__CHANGE_BREAKPOINT.dispatch(
-            e.target.value
-          )
-        }
+        onChange={onChangeBreakpoint}
         value={currentBreakpointId}
       />
     </VStackBox>
